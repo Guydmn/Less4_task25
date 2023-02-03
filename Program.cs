2e8ef91418db6c08dbec1804551714280d4747ee
Roman Guydmn <romanguydmn@gmail.com>
2023-02-03 21:19:06 +0400

    Задача25 Семинар 5

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double NumberPow (int P1, int P2)
+{
+    double x = Math.Pow(P1, P2);
+    return x;
+}
+
+Console.Write("Введите число A: ");
+int P1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int P2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Первое число в степени второго числа = {NumberPow(P1, P2)}");
